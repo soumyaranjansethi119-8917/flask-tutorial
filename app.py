27ce9d8908d48a614c9571c9595f97c0e038290e
soumyaranjansethi119-8917 <soumyaranjansethi119@gmail.com>
2026-09-08 23:49:28 +0530

    Add routing

--- app.py
+++ app.py
@@ -3,8 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     return 'Hello, World!'
+
+#routing for the about page
+@app.route('/about')
+def about():
+    return 'This is the about page.'
+
+#routing for the contact page
+@app.route('/contact')
+def contact():
+    return 'This is the contact page.'
 
 
 if __name__ == '__main__':
